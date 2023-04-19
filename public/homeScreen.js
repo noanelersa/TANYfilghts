1391c88e8fdceba120efac4d90c84219470bd847
Talia <taliamoshe06@cs.colman.ac.il>
2023-04-19 21:24:00 +0300

    About us using multiple columns + deals design + payment design + fix bugs

--- public/homeScreen.js
+++ public/homeScreen.js
@@ -17,6 +17,33 @@ try {
         e.preventDefault();
         $('#airportModalRegister').modal('show');
     });
+    $('#myButtonParis').click(function () {
+        $("#ParisModal").modal("hide");
+        $("#paymentModal").modal("show");
+
+    });
+    $('#myButtonLondon').click(function () {
+        $("#londonModal").modal("hide");
+        $("#paymentModal").modal("show");
+
+    });
+    $('#myButtonNewYork').click(function () {
+        $("#newYorkModal").modal("hide");
+        $("#paymentModal").modal("show");
+    });
+    function openPopup(){
+        const popup = document.getElementById("popup");
+        return popup.classList.add("open-popup");
+    }
+    function closePopup(){
+        const popup = document.getElementById("popup");
+        $('#paymentModal').modal('hide')
+        return popup.classList.remove("open-popup");
+        }
+
+    document.getElementById("openBtn").addEventListener("click",()=>{openPopup()});
+    document.getElementById("closeBtn").addEventListener("click",()=>{closePopup()});
+
 }
 catch (e) {}
 
