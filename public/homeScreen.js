9622ddc9e25398f9ccc88cd51c4481412620fb00
yaelhacshor <yaelhacshor@gmail.com>
2023-04-19 12:24:58 +0300

    added canvas with Animated Airplane

---
+++ public/homeScreen.js
@@ -0,0 +1,35 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var x = 0;
+
+var airplane = '\u2708';
+var airplaneWidth = c.measureText(airplane).width;
+
+var yMin = 50;
+var yMax = innerHeight - yMin;
+
+var y = Math.floor(Math.random() * (yMax - yMin)) + yMin;
+
+//var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+function animate(){
+    requestAnimationFrame(animate); // this creates a loop
+
+    c.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+
+    c.font = '64px Arial'; // draw the airplane
+    c.fillStyle = '#f0ffff';
+    c.fillText(airplane, x, y);
+
+    x += 2;
+
+    if ( x > innerWidth + airplaneWidth){ // if the airplane goes off the page, return to x coordinate of 0
+        x = 0;
+    }
+}
+
+animate();
